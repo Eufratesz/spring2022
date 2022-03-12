7cb402af1ead69e6ea5b33c4c88b6922ebecd30e
Eufratesz <lokoshazi@freemail.hu>
2022-03-12 20:42:38 +0100

    Modified

--- src/main/java/Basics/HelloWorld.java
+++ src/main/java/Basics/HelloWorld.java
@@ -3,6 +3,7 @@ package Basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        String message = "Hello World!";
+        System.out.println(message);
     }
 }
