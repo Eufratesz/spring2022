8e8e1147fd5d8dea2e3db8d31102bb22535bd791
Eufratesz <lokoshazi@freemail.hu>
2022-03-12 20:42:54 +0100

    First commit

---
+++ src/main/java/Basics/ScannerReadWrite.java
@@ -0,0 +1,26 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class ScannerReadWrite {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("Year of birth?");
+        int yearOfBirth = scanner.nextInt();
+        System.out.println("Year of birth: " + yearOfBirth);
+        int age = 2022 - yearOfBirth;
+        System.out.println("Age: " + age);
+
+        System.out.println("What's your favourite number?");
+        int number = scanner.nextInt();
+        System.out.println("What's your favourite season?");
+        scanner.nextLine();
+        String season = scanner.nextLine();
+        System.out.println("Favourite number: " + number);
+        System.out.println("Faourite season: " + season);
+    }
+}
