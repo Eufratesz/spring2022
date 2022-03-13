158d2d2639b80643ecd995659f67a95f5f57cf59
Eufratesz <lokoshazi@freemail.hu>
2022-03-13 07:10:53 +0100

    first commit

---
+++ src/main/java/classstructuremethods/NoteMain.java
@@ -0,0 +1,29 @@
+package classstructuremethods;
+
+import java.util.Scanner;
+
+public class NoteMain {
+
+    public static void main(String[] args) {
+
+        Note note = new Note();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name: ");
+        String name = scanner.nextLine();
+        note.setName(name);
+
+        System.out.println("Topic:");
+        String topic = scanner.nextLine();
+        note.setTopic(topic);
+
+        System.out.println("Text:");
+        String text = scanner.nextLine();
+        note.setText(text);
+
+        System.out.println(note.getNoteText());
+
+
+
+    }
+}
