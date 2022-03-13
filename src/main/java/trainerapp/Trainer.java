8d2e4f9331e5cca58e711fa7cbf9d7fb75297145
Eufratesz <lokoshazi@freemail.hu>
2022-03-13 07:11:15 +0100

    using constructor

--- src/main/java/trainerapp/Trainer.java
+++ src/main/java/trainerapp/Trainer.java
@@ -5,6 +5,11 @@ public class Trainer {
 
     private int yearOfBirth;
 
+    public Trainer(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
     public String getName(){
         return name;
     }
--- src/main/java/trainerapp/TrainerMain.java
+++ src/main/java/trainerapp/TrainerMain.java
@@ -2,26 +2,20 @@ package trainerapp;
 
 public class TrainerMain {
     public static void main(String[] args) {
-        Trainer trainer = new Trainer();
-        trainer.setName("John Doe");
-        trainer.setYearOfBirth(1980);
+        Trainer trainer = new Trainer("John Doe", 1980);
+        Trainer anotherTrainer = new Trainer("John Doe", 1980);
+
+
         System.out.println(trainer.getName());
         System.out.println(trainer.getYearOfBirth());
 
-        Trainer anotherTrainer = new Trainer();
-        anotherTrainer.setName("Jane Doe");
-        anotherTrainer.setYearOfBirth(1981);
         System.out.println(anotherTrainer.getName());
         System.out.println(anotherTrainer.getYearOfBirth());
-
-        System.out.println(anotherTrainer.getName());
-        System.out.println(anotherTrainer.getNameAndYearOfBirth());
 
         anotherTrainer.changeName("Joe Doe");
         System.out.println(anotherTrainer.getName());
 
-        anotherTrainer.setName("Jacky Doe");
-        System.out.println(anotherTrainer.getName());
+
 
     }
 }
