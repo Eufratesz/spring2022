a6ef8adba4cc0a2165a1b69785a873c211ba40ad
Eufratesz <lokoshazi@freemail.hu>
2022-03-20 06:36:46 +0100

    first commit

---
+++ src/main/java/array/finalmodifier/Week.java
@@ -0,0 +1,15 @@
+package array.finalmodifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Week {
+
+    public static final List<String> DAYS = Arrays.asList("hétfő", "kedd", "szerda", "csütörtök",
+            "péntek", "szombat", " vasárnap");
+
+    public static void main(String[] args) {
+        DAYS.set(1, "szerda");
+        System.out.println(DAYS);
+    }
+}
